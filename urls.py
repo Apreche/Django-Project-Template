39e0070cb1ab613c0c23317f2e3658d057cb3b1c
Scott Rubin <apreche@frontrowcrew.com>
2011-03-27 11:09:30 -0400

    fixed typos

--- urls.py
+++ urls.py
@@ -15,13 +15,13 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
-    if settings.DEBUG:
-        urlpatterns += patterns('',
-            url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-                'document_root': settings.MEDIA_ROOT,
-            }),
-        )
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )
 
 urlpatterns += staticfiles_urlpatterns()
