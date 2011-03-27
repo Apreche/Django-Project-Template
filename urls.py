a0fefb6e8aa4bd3395d0de89318351fcaa72c61b
Scott Rubin <apreche@frontrowcrew.com>
2011-03-27 11:01:06 -0400

    Added development media and static serve urls

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -14,4 +15,13 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    if settings.DEBUG:
+        urlpatterns += patterns('',
+            url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+                'document_root': settings.MEDIA_ROOT,
+            }),
+        )
 )
+
+urlpatterns += staticfiles_urlpatterns()
